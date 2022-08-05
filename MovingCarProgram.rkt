5be123be770368e6984c8e4d82cbf627aac8a182
Jesse Ogburn <jogburn88@gmail.com>
2022-08-05 07:43:44 -0400

    got basic program to work

--- MovingCarProgram.rkt
+++ MovingCarProgram.rkt
@@ -2,51 +2,46 @@
 ;; about the language level of this file in a form that our tools can easily process.
 #reader(lib "htdp-beginner-reader.ss" "lang")((modname MovingCarProgram) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp") (lib "batch-io.rkt" "teachpack" "2htdp")) #f)))
 ; physical constants
-(define WIDTH-OF-WORLD 200)
-(define WHEEL-RADIUS 5)
+(define WIDTH-OF-WORLD 500)
+(define WHEEL-RADIUS 3)
 (define WHEEL-DISTANCE (* WHEEL-RADIUS 5))
 
 ; graphical constants
+(define MOTORCAR (overlay/align/offset "right" "bottom" (circle (* WHEEL-RADIUS 3) "solid" "black") 3 (* WHEEL-RADIUS 0)
+                                       (overlay/align/offset "left" "bottom" (circle (* WHEEL-RADIUS 3) "solid" "black") -3 (* WHEEL-RADIUS -3)
+                                       (above (rectangle (* WHEEL-RADIUS 10) (* WHEEL-RADIUS 4) "solid" "red")
+                                              (rectangle (* WHEEL-RADIUS 20) (* WHEEL-RADIUS 5) "solid" "red")))))
+(define (TREE scale) (overlay/align "middle" "top" (circle (* scale 3) "solid" "green")
+                            (rectangle (* scale 2) (* scale 9) "solid" "brown")))
 
-(define WHEEL
-  (circle WHEEL-RADIUS "solid" "black"))
+(define BACKGROUND (overlay (TREE 5) (empty-scene WIDTH-OF-WORLD (+ 10 (image-height MOTORCAR)))))
 
-(define SPACE
-  (RECTANGLE ...WHEEL-RADIUS... "white"))
-
-(define BOTH-WHEELS
-  (beside WHEEL SPACE WHEEL))
-
-(define motorcar (overlay/align "middle" "bottom" (rectangle (* WHEEL-RADIUS 10) (* WHEEL-RADIUS 2) "solid" "red")
-                                                  (rectangle (* WHEEL-RADIUS 5) (* WHEEL-RADIUS 4) "solid" "red")))
-
-; a WorldState is a Number.
-; interpretation the number of pixels between
-; the left border of the scene and the car
-
-; render
-; clock-tick-handler
-; keystroke-handler
-; mouse-event-handler
-; end?
 
 ; WorldState -> Image
 ; places the image of the car x pixels from
 ; the left margin of the BACKGROUND image
 (define (render x)
-  BACKGROUND)
+  (place-image MOTORCAR (+ (/ (image-width MOTORCAR) 2) x) (+ (/ (image-height MOTORCAR) 2) 10) BACKGROUND))
 
-; WorldState -> WorldSate
-; adds 3 to x to move the car right
+; WorldState -> WorldState
+; moves the car by 3 pixels for every clock tick
+; examples:
+;  given: 20, expect 23
+;  given: 78, expect 81
+(define (clock-tick-handler cw)
+  (+ cw 3))
 
-(define (tock x)
-  x)
+; WorldState -> WorldState
+; detects when the program should end
+(define (end? x) (> x WIDTH-OF-WORLD))
 
 ; WorldState -> WorldState
 ; launches the program from some initial state
 (define (main ws)
   (big-bang ws
-    [on-tick tock]
-    [to-draw render]))
+    [on-tick clock-tick-handler]
+    [to-draw render]
+    [stop-when end?]))
 
-(main 13)
+(main 0)
+
